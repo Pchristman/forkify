7bcb9db9f80f8468f755eca942de6a34bff056f1
pchristman <pchristman1@outlook.com>
2022-08-09 13:48:58 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f6f99e9e-8d7a-41c9-ad11-e87c9e73ccec';
+export const MODAL_CLOSE_SEC = 2.5;
